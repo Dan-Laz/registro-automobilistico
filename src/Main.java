d4ff1027c779126f100c82561b4cb09f23a26adc
LAZZARONI.21130 <lazzaroni.daniel.studente@itispaleocpapa.it>
2025-05-19 13:45:09 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
+
+    public static HashMap<Targa, Proprietario> registro = new HashMap<Targa, Proprietario>();
+
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        HashMap<Targa, Proprietario> registro = new HashMap<Targa, Proprietario>();
+        do{
+            System.out.println("targa RETURN nome RETURN cognome RETURN codice fiscale RETURN");
+            if (put(new Targa(in.nextLine()), new Proprietario(in.nextLine(),in.nextLine(),in.nextLine()))){
+                System.out.println("aggiunto");
+            }else{
+                System.out.println("non aggiunto, gia presente");
+            }
 
-        registro.put(new Targa("CDWFRER"), new Proprietario("Dino","Cortinovis","DNCRT9884GB255"));
-        registro.put(new Targa("NMREIYF"), new Proprietario("Livio","Dentella","LVDNT84297632"));
-        registro.put(new Targa("CJMRERF"), new Proprietario("Alessio","Gritti","LSSGRT9865GF255"));
-        // copia
-        registro.put(new Targa("CJMRERF"), new Proprietario("Alessio","Gritti","LSSGRT9865GF255"));
+            System.out.println(registro);
+            System.out.println("continue? yes - no");
+        }while(in.nextLine().equals("yes"));
 
-        System.out.println(registro);
 
+    }
+
+    public static boolean put(Targa t, Proprietario p){
+        if (registro.containsKey(t)){
+            return false;
+        }else{
+            registro.put(t,p);
+            return true;
+        }
     }
 }
